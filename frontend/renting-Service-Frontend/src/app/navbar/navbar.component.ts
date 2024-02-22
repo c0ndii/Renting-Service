b35946e780e078e9714f69623b287ab07d58c79a
c0ndii <105825543+c0ndii@users.noreply.github.com>
2024-02-22 01:11:27 +0100

    feat(frontend): Navbar

--- frontend/renting-Service-Frontend/src/app/navbar/navbar.component.ts
+++ frontend/renting-Service-Frontend/src/app/navbar/navbar.component.ts
@@ -5,11 +5,12 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatSidenavModule, MatCheckboxModule, FormsModule, MatButtonModule, MatIconModule, MatMenuModule],
+  imports: [MatSidenavModule, MatCheckboxModule, FormsModule, MatButtonModule, MatIconModule, MatMenuModule, LoginComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
