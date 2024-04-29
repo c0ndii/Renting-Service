71125c2c38e2c7266c96886722b0a57f55c4b04a
c0ndii <105825543+c0ndii@users.noreply.github.com>
2024-04-29 15:10:39 +0200

    feat(frontend): Profile component

--- frontend/renting-Service-Frontend/src/app/profile/profile.component.ts
+++ frontend/renting-Service-Frontend/src/app/profile/profile.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [MatProgressSpinnerModule, MatButtonModule, MatButtonToggleModule, MatIconModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent implements OnInit {
+  username: string | undefined = '';
   constructor(private authService: AuthService, private router: Router) {
+    this.username = authService.getUserName();
   }
 
   ngOnInit(): void {
