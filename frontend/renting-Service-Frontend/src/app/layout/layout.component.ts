b1caab8f05809a671ee0e126ce43950900856bd3
c0ndii <105825543+c0ndii@users.noreply.github.com>
2024-05-08 22:40:40 +0200

    feat(frontend): MapImplementation + SSROff

--- frontend/renting-Service-Frontend/src/app/layout/layout.component.ts
+++ frontend/renting-Service-Frontend/src/app/layout/layout.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NavbarService } from '../services/navbar.service';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import * as Leaflet from 'leaflet';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [ RouterOutlet, NavbarComponent, CommonModule ],
+  imports: [ RouterOutlet, NavbarComponent, CommonModule, LeafletModule ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
@@ -15,5 +17,21 @@ export class LayoutComponent {
   constructor(private navbar: NavbarService){
     this.navbar.enableInputs();
   }
+  options: Leaflet.MapOptions = {
+    layers: getLayers(),
+    zoom: 12,
+    zoomControl: false,
+    center: new Leaflet.LatLng(52.13, 21.0)
+  };
+  readyUpMap(map: Leaflet.Map){
+    map.addControl(Leaflet.control.zoom({ position: 'bottomright' }));
+  }
   isLogged: boolean = false;
 }
+export const getLayers = (): Leaflet.Layer[] => {
+  return [
+    new Leaflet.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors'
+    } as Leaflet.TileLayerOptions),
+  ] as Leaflet.Layer[];
+};
